94b2265ff0427bc231c04150328e0820732f31bf
hashika <m.g.hashikamaduranga@gmail.com>
2020-04-01 23:09:45 +0530

    create a socket io server

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+const {saveMessage} =require('./dbConnection');
 
 const router = require('./router');
 
 const app = express();
-
+const server = http.createServer(app);
+const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+io.on('connect', (socket) =>{
+  
 
+  socket.on('disconnect', () => {
+   
+  })
+});
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
