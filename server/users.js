c6fd76cfa6a3376e79b7ade22eae09fd4786062c
hashika <m.g.hashikamaduranga@gmail.com>
2020-04-09 23:02:50 +0530

    create users js file for add users

---
+++ server/users.js
@@ -0,0 +1,19 @@
+const users=[];
+
+const addUser = ({ id, name, room }) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+  
+    const existingUser = users.find((user) => user.room === room && user.name === name);
+  
+    if(!name || !room) return { error: 'Username and room are required.' };
+    if(existingUser) return { error: 'Username is taken.' };
+  
+    const user = { id, name, room };
+  
+    users.push(user);
+     
+    return { user };
+  }
+
+  module.exports= {addUser}
