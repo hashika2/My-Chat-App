82af3ab111cd1565f8a4a84accec124753027ba8
hashika <m.g.hashikamaduranga@gmail.com>
2020-04-24 23:25:10 -0700

    fix issue of register user and get token

--- server/User.js
+++ server/User.js
@@ -1,26 +1,39 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-//   avatar: {
-//     type: String
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now
-//   }
-});
 
-module.exports = User = mongoose.model('user', UserSchema);
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique:true //two user cannot access same email
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+      }
+  }));
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email:Joi.string().min(5).max(255).required().email(),
+    password:Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
--- server/router.js
+++ server/router.js
@@ -1,95 +1,73 @@
+const config=require('config');
+const jwt=require('jsonwebtoken')
+const bcrypt=require('bcryptjs')
+const _ = require('lodash')
+const {User, validate} = require('./User');
+//const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-//const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
-
-const User = require('./User');
 
 const mongoose =require('mongoose');
 mongoose.connect('mongodb://localhost/mydb',console.log("mongodb connected"))
 
-router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
-});
+//register new user
+router.post('/', async (req, res) => {
+  const {error}=validate(req.body);
+  
+  if(error) return res.status(400).send(error.details[0].message);
 
-router.post(
-  '/',
-  [
-    check('name', 'Name is required')
-      .not()
-      .isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
-  ],
-  async (req, res) => {
-    
-    const errors = validationResult(req);
+  let user = await User.findOne({email:req.body.email})
+  console.log(user)
+  if(user){
+   return(
+     res.status(400).send('User already exist')
+     )
+   }
 
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
-    const { name, email, password } = req.body;
-    try {
-      let user = await User.findOne({ email });
+  //save user on the database
+  user =new User({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password
+  });
+  //console.log(user.email)
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
-      }
+  //set hash password
+  const salt=await bcrypt.genSalt(10);
+  user.password=await bcrypt.hash(user.password,salt);
 
-      // const avatar = gravatar.url(email, {
-      //   s: '200',
-      //   r: 'pg',
-      //   d: 'mm'
-      // });
-      console.log(req.body)
-      user = new User({
-        name,
-        email,
-         password,
-         //avatar,
-      });
-
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      jwt.sign(
-        payload,
-        "config.get('jwtSecret')",
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          console.log(token)
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
+  //user =new user()
+  await user.save();
+  const payload={
+    user:{
+      id:user.id
     }
+  }
+  // jwt.sign(payload,"jwtToken"),
+  // {expiresIn:360000},
+  // (err,token)=>{
+  //   if(err) throw err;
+  //   res.json({token})
+  // }
+
+  //set token
+  const token=jwt.sign({payload},"jwtPrivateKey",{expiresIn:'24h'})
+  console.log(token)
+  //res.send(_.pick(user,['_id','name','email']))
+  res.send(token)
+  //same as above(send details to the client)
+
+  // res.send({
+  //   name:user.name,
+  //   email:user.email
+  // })
   }
 );
 
-module.exports = router;
 
 router.post('/login',(req,res) =>{
   re
 })
 
 module.exports = router;
+
