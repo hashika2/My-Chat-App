24c24621312c440cc4f5d2f06d648790117fafc6
hashika <m.g.hashikamaduranga@gmail.com>
2020-07-16 13:55:04 -0700

    define ui for session timeout

--- client/src/components/timeoutSession/IdleTimeOut.js
+++ client/src/components/timeoutSession/IdleTimeOut.js
@@ -1,14 +1,31 @@
-import React,{ useRef } from 'react';
+import React,{ useRef, useState } from 'react';
 import IdleTimer from 'react-idle-timer';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 
 const IdleTimeOut = () =>{
     const idleTimerRef = useRef(null);
+    const [isLoggedIn,setIsLoggedIn] = useState(true);
+    const [modalIsOpen,setModalIsOpen] = useState(false);
     const onIdle = () => {
         console.log("user id idle");
+        setModalIsOpen(true);
     }
     return(
         <div>
+            {
+                isLoggedIn ? <h2>hello world</h2> :<h2>hello guest</h2>
+            }
+            <Modal isOpen={modalIsOpen}>
+                <h2> You have been idle for a while</h2>
+                <p> You have been logout soon</p>
+                <div>
+                    <button >Logout me</button>
+                    <button >Keep me up</button>
+                </div>
+            </Modal>
             <IdleTimer ref={idleTimerRef} timeout={5 * 1000} onIdle={onIdle}></IdleTimer>
         </div>
     )
