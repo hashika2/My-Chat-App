7212b5eaec85f10ab55f1b630d0f099816d52ba1
hashika <m.g.hashikamaduranga@gmail.com>
2020-10-24 12:57:55 +0530

    done upload image

--- server/server/deliveries/controller/upload/Upload.js
+++ server/server/deliveries/controller/upload/Upload.js
@@ -1,13 +1,44 @@
-const {UploadService} = require('../../../services/uploadsService');
+/* nee to clarrify */
 
-const Upload = (req,file) => {
-  const { username,email,password } = req.body;
-  if (validateResult.error) {
-    return res.status(400).json({ error: validateResult.error.details[0].message });
-  }
+require("dotenv").config();
+const crypto = require("crypto");
+const path = require("path");
+const mongoose = require("mongoose");
+const GridFsStorage = require("multer-gridfs-storage");
 
-    const uploadService = UploadService(req,file);
-    return uploadService;
+const mongoURI = "mongodb+srv://hashika:hashika@cluster0-qollh.mongodb.net/test?retryWrites=true&w=majority";
+const conn = mongoose.createConnection(mongoURI,{useUnifiedTopology: true,useNewUrlParser: true});
+
+ /* init gfs */
+let gfs;
+conn.once("open", () => {
+    /* init stream */
+    gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: "uploads"
+    });
+});
+
+const setStorage = () => {
+  /* Storage */
+  const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString("hex") + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: "uploads"
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+  });
+  return storage;
 }
 
-module.exports = { Upload }
+module.exports = { setStorage } ;
