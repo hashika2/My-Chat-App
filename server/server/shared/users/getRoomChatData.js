e79928c930e1d7b6fd451c6f09be3010e7b24cbc
hashika <m.g.hashikamaduranga@gmail.com>
2021-01-03 12:54:55 +0530

    add roo chat data js

---
+++ server/server/shared/users/getRoomChatData.js
@@ -0,0 +1,37 @@
+const {students,chats,officers,clients,developers} = require('../../../rooms/rooms');
+
+
+const getRoomChatData = (room,io) => {
+    if(room =="Students"){   
+        students.find((err,data)=>{
+          return io.emit("output message",data);      
+        })
+      }
+      if(room =="Officers"){
+        officers.find((err,data) => {
+          return io.emit("output message",data);
+        })
+      }
+      if(room =="Clients"){       
+        clients.find((err,data) => {
+          return io.emit("output message",data);
+        })
+      }
+      if(room =="Developers"){
+        developers.find((err,data) =>{
+          return io.emit("output message",data);
+        })
+      }
+      if(room =="Private"){
+        User.find((err,data) => {
+          return io.emit("output data",data);
+        })
+      }
+      else {
+        chats.find((err,data) =>{
+          return io.emit("output message",data);
+        })
+      }
+}
+
+module.exports = getRoomChatData;
