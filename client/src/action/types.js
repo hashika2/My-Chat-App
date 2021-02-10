1e8c6a1e290155c0e4b4bf3edc51d748d561629a
hashika <m.g.hashikamaduranga@gmail.com>
2021-02-10 14:52:28 +0530

    aad type

--- client/src/action/types.js
+++ client/src/action/types.js
@@ -1,4 +1,10 @@
 export default  {
     SET_ALERT: "SET_ALERT",
-    REMOVE_ALERT: "REMOVE_ALERT"
+    REMOVE_ALERT: "REMOVE_ALERT",
+    LOGGED_USER: "LOGGED_USER",
+    config :{
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    }
 }
