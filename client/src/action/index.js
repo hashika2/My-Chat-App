bf90b4dfb56b741fdec1c8a1fc13d78a1b80e199
hashika <m.g.hashikamaduranga@gmail.com>
2021-02-10 20:53:50 +0530

    change url and  add headers

--- client/src/action/index.js
+++ client/src/action/index.js
@@ -1,77 +1,93 @@
-import axios from 'axios';
-import{ setAlert} from './alert';
-import config from './types';
+import axios from "axios";
+import { setAlert } from "./alert";
+import environment from "../components/environment/env.json";
 
-export const roommed = ({name,room}) => async dispatch=> {
-    const res = {
-        name:name,
-        room:room
-    }
+const config = {
+  headers: {
+    "Content-Type": environment.Content_Type,
+    "x-browser": environment.x_browser,
+    "x-device": environment.x_device,
+  },
+};
+
+export const roommed = ({ name, room }) => async (dispatch) => {
+  const res = {
+    name: name,
+    room: room,
+  };
+  dispatch({
+    type: "ROOMED",
+    payload: res,
+  });
+};
+
+export const register = ({ name, email, password }) => async (dispatch) => {
+  const body = {
+    name,
+    email,
+    password,
+  };
+  try {
+    const res = await axios.post(`${environment.baseUrl}/user`, body, config);
+
     dispatch({
-        type:'ROOMED',
-        payload:res
-    })
-}
-
-export const register=({name,email,password}) => async dispatch => {
-    const body = {
-        name,email,password
-    }
-    try{
-        const res = await axios.post('http://localhost:5000/api/user',body,config);
-
-        dispatch({
-            type:"USER_REGISTERED",  
-            payload:res.data
-        })
-        alertData(res.data);
-    }catch(error){
-        if(error){
-            console.log("error "+error)
-            dispatch(setAlert(error,'danger'))
-        }
-        dispatch({
-            type:"REGISTER_FAIL"
-        })
-    }
-}
-
-const alertData = (data)=>async dispatch => {
-        dispatch({
-            type:"ALERTMESSAGE",
-            payload:data
-        })
-}
-
-export const login=({email,password}) => async dispatch => {
-    const body = {
-        email,password
-    }
-    try{
-        const res = await axios.post('http://localhost:5000/api/user/login',body,config);   
-        dispatch({  
-            type:"USER_LOGGED",
-            payload:res.data
-        })
-    }catch(error){
-        if(error){
-            console.log("error   "+error)
-            // error.map(error => { dispatch(setAlert(error,'danger'))});
-            dispatch(setAlert(error,'danger'));
-        }
-        dispatch({
-            type:"LOGIN_FAIL"
-        })
-    }
-    
-    
-}
-export const getRoomData = (room) => async dispatch => {
-    const body = { room }
-    const res = await axios.get("http://localhost:5000/api/user/roomData",body,config);
-    dispatch({
-        type:"GET_ROOM_DATA",
-        payload: res.data     
+      type: "USER_REGISTERED",
+      payload: res.data,
     });
-}
+    alertData(res.data);
+  } catch (error) {
+    if (error) {
+      console.log("error " + error);
+      dispatch(setAlert(error, "danger"));
+    }
+    dispatch({
+      type: "REGISTER_FAIL",
+    });
+  }
+};
 
+const alertData = (data) => async (dispatch) => {
+  dispatch({
+    type: "ALERTMESSAGE",
+    payload: data,
+  });
+};
+
+export const login = ({ email, password }) => async (dispatch) => {
+  const body = {
+    email,
+    password,
+  };
+  try {
+    const res = await axios.post(
+      `${environment.baseUrl}/user/login`,
+      body,
+      config
+    );
+    dispatch({
+      type: "USER_LOGGED",
+      payload: res.data,
+    });
+  } catch (error) {
+    if (error) {
+      console.log("error   " + error);
+      // error.map(error => { dispatch(setAlert(error,'danger'))});
+      dispatch(setAlert(error, "danger"));
+    }
+    dispatch({
+      type: "LOGIN_FAIL",
+    });
+  }
+};
+export const getRoomData = (room) => async (dispatch) => {
+  const body = { room };
+  const res = await axios.get(
+    "http://localhost:5000/api/user/roomData",
+    body,
+    config
+  );
+  dispatch({
+    type: "GET_ROOM_DATA",
+    payload: res.data,
+  });
+};
