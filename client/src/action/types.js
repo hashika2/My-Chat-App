7c4da216d3425a2bb138fdc56747395a305621f0
hashika <m.g.hashikamaduranga@gmail.com>
2021-02-10 20:53:14 +0530

    change types

--- client/src/action/types.js
+++ client/src/action/types.js
@@ -1,10 +1,5 @@
-export default  {
-    SET_ALERT: "SET_ALERT",
-    REMOVE_ALERT: "REMOVE_ALERT",
-    LOGGED_USER: "LOGGED_USER",
-    config :{
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    }
-}
+export default {
+  SET_ALERT: "SET_ALERT",
+  REMOVE_ALERT: "REMOVE_ALERT",
+  LOGGED_USER: "LOGGED_USER",
+};
